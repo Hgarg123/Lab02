a2d03f464f792bf481777ff44fc91dd78bd3f33e
Harshit <harshit.garg_cs18@gla.ac.in>
2019-08-22 15:00:08 +0530

    add file Main.java

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,17 @@
 package main;
 
+import java.util.Scanner;
+
 /**
  * @author Harshit Garg
  * @version 1.0
- */
-
+ * */
 public class Main {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        String s = obj.nextLine();
+        System.out.println("Welcome," + s);
+
+
+    }
 }
